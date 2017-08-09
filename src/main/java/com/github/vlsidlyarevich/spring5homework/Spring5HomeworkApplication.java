bcd6109d83267c7c8330a54c8f71a6ef91addead
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-08-09 21:44:14 +0300

    Initial commit

---
+++ src/main/java/com/github/vlsidlyarevich/spring5homework/Spring5HomeworkApplication.java
@@ -0,0 +1,12 @@
+package com.github.vlsidlyarevich.spring5homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5HomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5HomeworkApplication.class, args);
+	}
+}
